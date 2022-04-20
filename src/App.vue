66f0b0b37d4a1f11f88685cbb20d3d0747776804
Vinicius Borges <vinibr45@gmail.com>
2022-04-20 19:48:43 -0300

    Subindo Alura no Git

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input
+      type="search"
+      class="filtro"
+      @input="filtro = $event.target.value"
+      placeholder="filtre o seu titulo!"
+    />
+
+    <ul class="lista-fotos">
+      <li
+        class="lista-fotos-item"
+        v-for="foto in fotosComFiltro"
+        :key="foto.titulo"
+      >
+        <new-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+        </new-painel>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import newPainel from "./components/shared/painel/newPainel.vue";
+import ImagemResponsiva from "./components/shared/imagem-responsiva/ImagemResponsiva.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    newPainel,
+    ImagemResponsiva,
+  },
+
+  data() {
+    return {
+      titulo: "Alurapic",
+      fotos: [],
+      filtro: "",
+    };
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter((foto) => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    },
+  },
+
+  created() {
+    let promise = this.$http.get("http://localhost:3000/v1/fotos");
+    promise.then((res) => res.json()).then((fotos) => (this.fotos = fotos));
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+.filtro {
+  display: block;
+  width: 100%;
 }
 </style>
